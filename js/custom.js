ad4fcdbe3148475e4bb17799cff6a284e8a28c8c
S1gma <9921822+s1gma@user.noreply.gitee.com>
2023-04-17 22:38:06 +0800

    Site updated: 2023-04-17 22:38:05

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+if(document.getElementsByClassName("category-title is-center")[0]){
+    document.getElementsByClassName("category-title is-center")[0].innerHTML = '';
+}
+if(document.getElementsByClassName("tag-cloud-title is-center")[0]){
+    document.getElementsByClassName("tag-cloud-title is-center")[0].innerHTML = '';
+}
